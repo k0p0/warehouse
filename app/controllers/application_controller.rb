347f4b1799027da4ffeb74ef7ef9651e618402a7
Olivier Bellier <olivier@observices.fr>
2017-08-14 16:14:12 +0200

    adding seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-   #  before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
- # def configure_permitted_parameters
- #    # For additional fields in app/views/devise/registrations/new.html.erb
- #    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address])
+ def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address])
 
- #   # For additional in app/views/devise/registrations/edit.html.erb
- #    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
- #  end
+   # For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 
 end
